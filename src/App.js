c7477538978b5f27e1e7f76f9e8df626d6c2591d
aileenunez <88999968+aileenunez@users.noreply.github.com>
2021-11-20 17:52:42 -0800

    testing different news api(com)

--- src/App.js
+++ src/App.js
@@ -8,18 +8,32 @@ import Articles from './Articles';
 function App() {
   const [news, setNews] = useState([])
   useEffect(function(){
-    const apiKey = "bfad15ce5ca44d5bb1289592477dc174"
+    // const apiKey = "bfad15ce5ca44d5bb1289592477dc174"
+    // axios({
+    //   url: "https://newsapi.org/v2/top-headlines", 
+    //   method: "GET", 
+    //   dataResponse: "json", 
+    //   params:{
+    //     apiKey: apiKey, 
+    //     language: 'en',
+    //   }, 
+    // }).then(function(response){
+    //   console.log(response.data.articles)
+    //   setNews(response.data.articles)
+    // })
+
+    const apiKey = "SIWrEacA2Futxp7zNsPEXpiVLU9JH0408Ot04O7L"
     axios({
-      url: "https://newsapi.org/v2/top-headlines", 
-      method: "GET", 
-      dataResponse: "json", 
-      params:{
-        apiKey: apiKey, 
-        language: 'en',
+      url: 'https://api.thenewsapi.com/v1/news/top', 
+      method: 'GET', 
+      dataResponse: 'json', 
+      params: {
+        api_token: apiKey, 
+        language: 'en', 
       }, 
     }).then(function(response){
-      console.log(response.data.articles)
-      setNews(response.data.articles)
+      console.log(response.data.data)
+      setNews(response.data.data)
     })
    
   }, []); 
@@ -31,19 +45,35 @@ function App() {
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptates qui dolores sit numquam quisquam architecto, magni consequuntur quo libero ut voluptate necessitatibus eveniet doloribus eaque. Voluptates modi quos in?</p>
       <Input/>
       {
-        news.map(function(articlez){ 
+
+          news.map(function(articlez){ 
           return(
             <>
           <Articles
+          key ={articlez.uuid}
           title = {articlez.title}
-          img = {articlez.urlToImage}
+          img = {articlez.image_url}
           alt = {articlez.title}
           url = {articlez.url}
           description = {articlez.description}
-          author = {articlez.author}
+          author = {articlez.source}
           />
           </>
           )})
+
+        // news.map(function(articlez){ 
+        //   return(
+        //     <>
+        //   <Articles
+        //   title = {articlez.title}
+        //   img = {articlez.urlToImage}
+        //   alt = {articlez.title}
+        //   url = {articlez.url}
+        //   description = {articlez.description}
+        //   author = {articlez.author}
+        //   />
+        //   </>
+        //   )})
     
       }
       <footer>Created at Juno College Technology</footer>
